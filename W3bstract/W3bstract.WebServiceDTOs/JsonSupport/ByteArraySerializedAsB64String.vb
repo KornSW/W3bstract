5c9bda6b3a5526b850116a391aaa1eae9eddc643
T. Korn <email@tobiaskorn.de>
2020-07-29 16:48:33 +0200

    new Artefact JsonConverter:'ByteArraySerializedAsB64String'

---
+++ W3bstract/W3bstract.WebServiceDTOs/JsonSupport/ByteArraySerializedAsB64String.vb
@@ -0,0 +1,30 @@
+﻿Imports System
+Imports Newtonsoft.Json
+
+''' <summary>
+''' use with Attribute: JsonConverter(GetType(ByteArraySerializedAsB64String))
+''' </summary>
+Public Class ByteArraySerializedAsB64String
+  Inherits JsonConverter
+
+  Public Overrides Function CanConvert(objectType As Type) As Boolean
+    Return (objectType = GetType(Byte()))
+  End Function
+
+  Public Overrides Sub WriteJson(writer As JsonWriter, value As Object, serializer As JsonSerializer)
+    Dim rawString As String = Convert.ToBase64String(DirectCast(value, Byte()))
+    writer.WriteValue(rawString)
+  End Sub
+
+  Public Overrides Function ReadJson(reader As JsonReader, objectType As Type, existingValue As Object, serializer As JsonSerializer) As Object
+    Dim rawString As String = reader.ReadAsString()
+    Dim value As Byte()
+    If (String.IsNullOrWhiteSpace(rawString)) Then
+      value = {}
+    Else
+      value = Convert.FromBase64String(rawString)
+    End If
+    Return value
+  End Function
+
+End Class
